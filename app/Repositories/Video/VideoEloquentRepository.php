dd26ad8e8b47eff5f8313f5744d82641f6341f60
CSN\admin <tt.hau94@gmail.com>
2018-10-11 11:09:52 +0700

    no message

---
+++ app/Repositories/Video/VideoEloquentRepository.php
@@ -0,0 +1,136 @@
+<?php
+namespace App\Repositories\Video;
+
+use App\Repositories\EloquentRepository;
+use DB;
+use App\Library\Helpers;
+
+class VideoEloquentRepository extends EloquentRepository implements VideoRepositoryInterface
+{
+    /**
+     * get model
+     * @return string
+     */
+    public function getModel()
+    {
+        return \App\Models\MusicModel::class;
+    }
+    /**
+     * Get all posts only published
+     * @return mixed
+     */
+    public function getAllPublished()
+    {
+        $result = $this->_model->where('is_published', 1)->get();
+
+        return $result;
+    }
+
+    /**
+     * Get post only published
+     * @param $id int Post ID
+     * @return mixed
+     */
+    public function findOnlyPublished($id)
+    {
+        $result = $this
+            ->_model
+            ->where('id', $id)
+            ->where('is_published', 1)
+            ->first();
+
+        return $result;
+    }
+
+    public function findOnlyMusicUrl($url)
+    {
+        $result = $this
+            ->_model
+            ->where('music_title_url', $url)
+            ->first();
+
+        return $result;
+    }
+    public function findOnlyMusicId($id)
+    {
+        $result = $this
+            ->_model
+            ->where('music_id', $id)
+            ->with('musicKara')
+            ->first();
+
+        return $result;
+    }
+    public function findMusicIds($ids)
+    {
+        $result = $this
+            ->_model
+            ->whereIn('music_id', $ids)
+            ->get();
+
+        return $result;
+    }
+    public function incrementCol($id, $field)
+    {
+        $result = $this
+            ->_model
+            ->where('music_id', $id)
+            ->Increment($field);
+        return $result;
+    }
+    public function suggestion($music, $type = 'music') {
+        $model = $this->_model;
+        if($type == 'video') {
+            $model = app()->make(
+                \App\Models\VideoModel::class
+            );
+        }
+        $pathDir = resource_path() . '/views/cache/suggestion/' . ceil($music->music_id / 1000) . '/';
+        $file = $pathDir . $music->music_id . '.blade.php';
+        if (!file_exists($pathDir)) {
+            mkdir($pathDir, 0777, true);
+        }else{
+            if(file_exists($file)) {
+                // update time to file case
+                if((time() - filemtime($file)) < UPDATE_CASE_SUGGESTION_MUSIC || UPDATE_CASE_SUGGESTION_MUSIC_ONCE) {
+                    return false;
+                }
+            }
+        }
+        $cache = $model->where('music_title', 'like', '%' . $music->music_title . '%')
+            ->where('music_id', '!=', $music->music_id)
+            ->select('music_id', 'cat_id', 'cat_level', 'cover_id', 'music_title_url', 'music_title', 'music_artist', 'music_album_id', 'music_listen', 'music_shortlyric', 'music_bitrate', 'music_filename')
+            ->limit(5)
+            ->orderBy('music_listen', 'desc')->get()->toArray();
+        $artists = explode(';', $music->music_artist);
+        $cache2 = $model->whereIn('music_artist', $artists)
+            ->where('music_id', '!=', $music->music_id)
+            ->select('music_id', 'cat_id', 'cat_level', 'cover_id', 'music_title_url', 'music_title', 'music_artist', 'music_album_id', 'music_listen', 'music_shortlyric', 'music_bitrate', 'music_filename')
+            ->limit((count($cache) >= 3 ? 15 : 20))
+            ->orderBy('music_listen', 'desc')->get()->toArray();
+        $video = [];
+        if($type = 'video') {
+            $video = \App\Models\VideoModel::where('music_title', $music->music_title)
+                ->where('music_artist', $music->music_artist)
+                ->select('music_id', 'cat_id', 'cat_level', 'cover_id', 'music_title_url', 'music_title', 'music_artist', 'music_album_id', 'music_listen', 'music_shortlyric', 'music_bitrate', 'music_filename')
+                ->orderBy('music_listen', 'desc')->get()->toArray();
+        }
+
+        $cacheTotal = array_merge($cache2, $cache);
+        $pathDir = resource_path() . '/views/cache/suggestion/' . ceil($music->music_id / 1000) . '/';
+        file_put_contents($pathDir . $music->music_id . '.blade.php',
+            '<?php 
+if ( !ENV(\'IN_PHPBB\') )
+{
+    die(\'Hacking attempt\');
+    exit;
+}
+global $sug;
+global $video;
+    
+$sug = ' . var_export($cacheTotal, true) . ';
+$video = ' . var_export($video, true) . ';
+?>');
+    }
+}
+
