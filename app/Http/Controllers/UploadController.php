6b4a628560777598ce5d4dd8024a32caa3d8495c
CSN\admin <tt.hau94@gmail.com>
2018-12-25 15:34:10 +0700

    update source

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -230,7 +230,7 @@ class UploadController extends Controller
         if(!$result)
             return redirect()->route('upload.createMusic')->with('error', 'tạo '.$mess.' thất bại');
         $fileName = $result->music_id.'.'.last(explode('.', $request->input('drop_files')));
-        Storage::disk('public')->move(DEFAULT_STORAGE_CACHE_MUSIC_PATH.$request->input('drop_files'), SOURCE_STORAGE_PATH.$fileName);
+        Storage::disk('public')->move(DEFAULT_STORAGE_CACHE_MUSIC_PATH.$request->input('drop_files'), Helpers::file_path($result->music_id, SOURCE_STORAGE_PATH, true).$fileName);
         $result->music_filename_upload = $fileName;
         $result->save();
         return redirect()->route($typeUpload == 'music' ? 'upload.createMusic' : 'upload.createVideo')->with('success', 'Đã tạo '.$mess.' ' . $csnMusic['music_title'] . '<a href="/dang-tai/'.($typeUpload == 'music' ? 'nhac' : 'video').'/'.$result->music_id.'"> quay lại chỉnh sửa</a>');
@@ -310,7 +310,7 @@ class UploadController extends Controller
             $csnMusic['music_filesize'] = $fileSize[$key];
             $result = $this->uploadRepository->create($csnMusic);
             $fileName = $result->music_id.'.'.last(explode('.', $item));
-            Storage::disk('public')->move(DEFAULT_STORAGE_CACHE_MUSIC_PATH.$item, SOURCE_STORAGE_PATH.$fileName);
+            Storage::disk('public')->move(DEFAULT_STORAGE_CACHE_MUSIC_PATH.$item, Helpers::file_path($result->music_id, SOURCE_STORAGE_PATH, true).$fileName);
             $result->music_filename_upload = $fileName;
             $result->save();
         }
