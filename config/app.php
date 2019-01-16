845c17271dda3ce1e3f95f4923a8b54d51ef7960
CSN\admin <tt.hau94@gmail.com>
2019-01-16 15:51:12 +0700

    any deny env debug

--- config/app.php
+++ config/app.php
@@ -251,5 +251,10 @@ return [
         'CRUD'  => Backpack\CRUD\CrudServiceProvider::class,
 
     ],
+    'debug_blacklist' => [
+        '_COOKIE' => array_keys($_COOKIE),
+        '_SERVER' => array_keys($_SERVER),
+        '_ENV' => array_keys($_ENV),
+    ],
 
 ];
