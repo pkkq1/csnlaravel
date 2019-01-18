e8f2785914f0df88682043b0d2bc4d69cecafdd9
TUANTRINH <trinhlt@gmail.com>
2019-01-18 18:32:26 +0700

    no message

--- app/Http/Controllers/Sync/SolrSyncController.php
+++ app/Http/Controllers/Sync/SolrSyncController.php
@@ -92,7 +92,7 @@ class SolrSyncController extends Controller
             die('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"><html><head><script type="text/javascript">window.location = "?m_start='. $item->music_id .'"; </script></head><body></body></html>');
         }
         else{
-            die('Done! Full Data!')
+            die('Done! Full Data!');
         }
         return response(['Ok']);
     }
