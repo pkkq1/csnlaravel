22d496d5b5695896e9ffa0db3480d082fe1b29a0
CSN\admin <tt.hau94@gmail.com>
2019-01-21 15:36:28 +0700

    update solarium

--- config/solarium.php
+++ config/solarium.php
@@ -6,6 +6,7 @@ return [
             'host' => env('SOLR_HOST', '127.0.0.1'),
             'port' => env('SOLR_PORT', '8983'),
             'path' => env('SOLR_PATH', '/solr/'),
+            'timeout' => 500,
             'core' => env('SOLR_CORE', 'collection1')
         ]
     ]
