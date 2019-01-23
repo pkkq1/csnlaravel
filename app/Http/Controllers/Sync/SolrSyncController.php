9ec906b7d5f4b03b5ee64020ddcf5dea95604936
TUANTRINH <trinhlt@gmail.com>
2019-01-23 11:44:41 +0700

    repair search

--- app/Http/Controllers/Sync/SolrSyncController.php
+++ app/Http/Controllers/Sync/SolrSyncController.php
@@ -55,7 +55,7 @@ class SolrSyncController extends Controller
                 ->where('music_deleted', '<', 1)
                 ->where('music_id', '>', intval($_GET['m_start']))
                 ->offset(0)
-                ->limit(2000)
+                ->limit(10000)
                 ->get();
         }
         DB::disconnect('mysql');
@@ -71,11 +71,11 @@ class SolrSyncController extends Controller
                 'id' => 'music_'.$item->music_id,
                 'music_title' => $item->music_title,
                 'music_title_search' => $titleSearch,
-                'music_title_artist_search' => $titleSearch,
-                'music_artist_search' => $artistSearch .' '. $artistSearch,
+                'music_title_artist_search' => $titleSearch . ' ' . $artistSearch,
+                'music_artist_search' => $artistSearch,
                 'music_title_charset_nospace' => str_replace(' ', '', $titleCharset),
                 'music_artist_charset_nospace' => str_replace(' ', '', $artistCharset),
-                'music_title_artist_charset_nospace' => str_replace(' ', '', $titleCharset) . ' ' . str_replace(' ', '', $artistCharset),
+                'music_title_artist_charset_nospace' => str_replace(' ', '', $titleCharset) . '' . str_replace(' ', '', $artistCharset),
                 'music_title_charset' => $titleCharset,
                 'music_artist_charset' => $artistCharset,
                 'music_title_artist_charset' => $titleCharset . ' '. $artistCharset,
@@ -148,7 +148,7 @@ class SolrSyncController extends Controller
                 'video_title_artist_search' => $titleSearch .' '. $artistSearch,
                 'video_title_charset_nospace' => str_replace(' ', '', $titleCharset),
                 'video_artist_charset_nospace' => str_replace(' ', '', $artistCharset),
-                'video_title_artist_charset_nospace' => str_replace(' ', '', $titleCharset) .' '. str_replace(' ', '', $artistCharset),
+                'video_title_artist_charset_nospace' => str_replace(' ', '', $titleCharset) .''. str_replace(' ', '', $artistCharset),
                 'video_title_charset' => $titleCharset,
                 'video_artist_charset' => $artistCharset,
                 'video_title_artist_charset' => $titleCharset .' '. $artistCharset,
