6d47f779376d357a842fe06468df3399195ef549
CSN\admin <tt.hau94@gmail.com>
2019-01-24 11:09:31 +0700

    update

--- resources/views/mobile/jwplayer/music.blade.php
+++ resources/views/mobile/jwplayer/music.blade.php
@@ -256,7 +256,7 @@ if($musicSet['type_listen'] == 'playlist') {
                                                 @if(isset($lyric_array['lyric']))
                                                     @if(isset($lyric_array['sub']) && $lyric_array['sub'] != false)
                                                         <div class="nav-link form-group form-check mb-0 autoplay sub-text" style="padding-left: 0px;">
-                                                            <input type="checkbox" class="form-check-input display-sub" id="display-sub" onclick="display_sub()">
+                                                            <input type="checkbox" class="form-check-input display-sub" checked id="display-sub" onclick="display_sub()">
                                                             <label class="form-check-label d-flex align-items-center" for="display-sub">
                                                                 <span class="switch"><span class="switch-inner"></span></span>
                                                                 <span style="font-size: 15px; color: #4b4b4b; font-family: 'SFProDisplay-Medium'; margin-left: 10px" class="txt">Hiển Thị Sub</span>
@@ -942,6 +942,7 @@ if($musicSet['type_listen'] == 'playlist') {
     });
     //// display Sub
     var displaySub = $('.sub_line');
+    displaySub.css('display', 'block');
     if(sessionStorage.getItem("display_sub") == 'true') {
         displaySub.css('display', 'block');
         $('#display-sub').attr("checked", "checked");
