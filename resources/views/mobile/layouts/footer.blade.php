0b693fd70c6b349da4ae06f205f16d8a0efff970
TUANTRINH <trinhlt@gmail.com>
2019-01-31 18:40:06 +0700

    banner

--- resources/views/mobile/layouts/footer.blade.php
+++ resources/views/mobile/layouts/footer.blade.php
@@ -226,3 +226,6 @@ use App\Library\Helpers;
     //     document.getElementById("search_autocomplete").trigger('focus');
     // })
 </script>
+
+<!-- BlueSeed - asynchronous code for placement 1849 CHIASENHAC.VN - mobile native button -->
+<script id="jshd1pm_1849" src="https://d2.blueseed.tv/ads-sync.js?placement=1849"></script>
