db0618b095e216fec12159d476e683933fb31d3e
CSN\admin <tt.hau94@gmail.com>
2019-02-02 23:32:24 +0700

    update

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -355,8 +355,8 @@ class UploadController extends Controller
             $album->save();
             if($request->input('album_cover')) {
 //                $typeImageCover = array_last(explode('.', $_FILES['choose_album_cover']['name']));
-                $fileNameCovert = Helpers::saveBase64Image($request->input('album_cover'), Helpers::file_path($album->cover_id, AVATAR_ALBUM_CROP_PATH, true), $album->cover_id);
-                Helpers::copySourceImage($request->file('choose_album_cover'), Helpers::file_path($album->cover_id, COVER_ALBUM_SOURCE_PATH, true), $album->cover_id);
+                $fileNameCovert = Helpers::saveBase64Image($request->input('album_cover'), Helpers::file_path($album->cover_id, AVATAR_ALBUM_CROP_PATH, true), $album->cover_id, 'jpg');
+                Helpers::copySourceImage($request->file('choose_album_cover'), Helpers::file_path($album->cover_id, COVER_ALBUM_SOURCE_PATH, true), $album->cover_id, 'jpg');
             }
             // update upload
             $upload = $album->upload;
@@ -420,8 +420,8 @@ class UploadController extends Controller
         if(!$album)
             return redirect()->route('upload.upload_album')->with('error', 'tạo album thất bại');
 //        $typeImageCover = array_last(explode('.', $_FILES['choose_album_cover']['name']));
-        $fileNameCovert = Helpers::saveBase64Image($request->input('album_cover'), Helpers::file_path($album->cover_id, AVATAR_ALBUM_CROP_PATH, true), $album->cover_id);
-        Helpers::copySourceImage($request->file('choose_album_cover'), Helpers::file_path($album->cover_id, COVER_ALBUM_SOURCE_PATH, true), $album->cover_id);
+        $fileNameCovert = Helpers::saveBase64Image($request->input('album_cover'), Helpers::file_path($album->cover_id, AVATAR_ALBUM_CROP_PATH, true), $album->cover_id, 'jpg');
+        Helpers::copySourceImage($request->file('choose_album_cover'), Helpers::file_path($album->cover_id, COVER_ALBUM_SOURCE_PATH, true), $album->cover_id, 'jpg');
         $album->cover_filename = $fileNameCovert;
         $album->save();
 //        // update solr
