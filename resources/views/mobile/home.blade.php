3b7b8a37851b98fbb28c96fac0597d13c83af3c4
TUANTRINH <trinhlt@gmail.com>
2019-02-14 14:25:06 +0700

    no message

--- resources/views/mobile/home.blade.php
+++ resources/views/mobile/home.blade.php
@@ -18,7 +18,10 @@ $catalog = config('constants.catalog');
     @include('cache.def_home_album_cat')
     <div class="header">
         <div class="header_top">
-            <nav class="navbar navbar-expand-lg navbar-dark flex-row-reverse"><a href="#" class="navbar-brand text-white button_search"><i aria-hidden="true" class="fa fa-search"></i></a><a href="/" class="navbar-brand logo"><img src="/images/logo-header.png" alt="logo"></a>
+            <nav class="navbar navbar-expand-lg navbar-dark flex-row-reverse">
+                <a href="#" class="navbar-brand text-white button_search"><i aria-hidden="true" class="fa fa-search"></i></a>
+                <a href="http://chiasenhac.vn/app/csn.html" target="_blank"><img src="/imgs/csn_app.png"></a>
+                <a href="/" class="navbar-brand logo"><img src="/images/logo-header.png" alt="logo"></a>
                 <button type="button" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
             </nav>
         </div>
