9eca219f296eb2a390d42e21306c9efa89206b38
CSN\admin <tt.hau94@gmail.com>
2019-02-15 10:58:56 +0700

    no message

--- app/Library/helpers.php
+++ app/Library/helpers.php
@@ -409,7 +409,7 @@ class Helpers
 
     public static function cover_url($cover_id = 0)
     {
-        return ($cover_id > 0) ? self::cover_path($cover_id) . $cover_id . '.jpg' : '/imgs/no_cover.jpg';
+        return ($cover_id > 0) ? self::cover_path($cover_id) . $cover_id . '.jpg' :  env('APP_URL').'/imgs/no_cover.jpg';
     }
     public static function artist_cover($artist_id)
     {
