0e5b4ea6b0f67e8185f1b0ded52bccb7a1760410
DESKTOP-ENHS70G\Admin <tt.hau94@gmail.com>
2019-02-16 16:36:01 +0700

    update

--- public/js/dropzone.js
+++ public/js/dropzone.js
@@ -224,7 +224,7 @@ var Dropzone = function (_Emitter) {
          * the event `maxfilesexceeded` will be called. The dropzone element gets the
          * class `dz-max-files-reached` accordingly so you can provide visual feedback.
          */
-        maxFilesize: 256,
+        maxFilesize: 4560,
 
         /**
          * The name of the file param that gets transferred.
