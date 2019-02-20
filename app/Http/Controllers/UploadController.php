f9c878cfae0b7e62e6623e1d7be7f647be5a86e0
CSN\admin <tt.hau94@gmail.com>
2019-02-20 18:05:47 +0700

    update

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -116,6 +116,8 @@ class UploadController extends Controller
                 return view('errors.text_error')->with('message', 'Danh mục trang bạn chọn không chính xác');
             if($music->cover_id)
                 $album = $this->coverRepository->findCover($music->cover_id);
+                if(!$album)
+                    return view('errors.text_error')->with('message', 'Tình trạng album không tìm thấy hoặc đang được xử lý');
         }
         return view('upload.upload_music', compact('typeUpload', 'music', 'album'));
     }
