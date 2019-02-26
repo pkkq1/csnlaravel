b50868492d0cfcf846d1c81fe1cb59b3fbec9f0c
TUANTRINH <trinhlt@gmail.com>
2019-02-26 10:50:55 +0700

    ads

--- resources/views/mobile/layouts/header_top.blade.php
+++ resources/views/mobile/layouts/header_top.blade.php
@@ -12,14 +12,3 @@ $Agent = new Agent();
         <button type="button" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
     </nav>
 </div>
-
-@if ( 0 )
-<main class="main" style="position: relative;">
-<div style="position: relative; text-align: center;">
-    <!-- Blueseed- Asynchronous Code for Ad Placement 1813 Chiasenhac Masthead Mobile Deleted -->
-    <ins class="hadar-placement" data-hdpm="1813"></ins>
-    <script id="jshd1pm_1813" src="https://static.blueseed.tv/ajs/display.js"></script>
-    <img src="http://data.chiasenhac.com/banner/banner_beta_300x150.jpg" height="150px">
-</div>
-</main>
-@endif
