02de4575da516af3cfd9fd2cd7f29816c937e875
TUANTRINH <trinhlt@gmail.com>
2019-04-04 08:47:17 +0700

    app

---
+++ public/app/gioithieu.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0" name="viewport">
+<link rel="stylesheet" href="http://data.chiasenhac.vn/demo2018/common/css/common.css" media="screen" />
+<title>Giới thiệu</title>
+</head>
+<body>
+<div class="container">
+	<p class="text-align-left border-bottom" style="padding-bottom: 10px;">
+		<img src="https://beta.chiasenhac.com/imgs/logo-web-official.png" width="60" style="box-shadow: 0px 0px 2px #eee;border-radius: 6px;" />
+		<span style="margin-left: 10px;
+			    display: inline-block;
+			    vertical-align: middle;
+			    width: calc(100% - 74px);">
+			<font color="#FF2D55">Ứng dụng nghe nhạc Chia Sẻ Nhạc</font></br>
+			<b style="font-size:10px; font-style: normal;"><i>Phiên bản <?php if (isset($_GET['v'])) { echo $_GET['v']; } ?></i></b>
+		</span>
+	</p>
+	<p class="text-align-justify border-bottom" style="padding-bottom: 10px;" text-align: justify>
+	<b>Chia Sẻ Nhạc</b> là cộng đồng chia sẻ âm nhạc chất lượng cao và xếp hạng âm nhạc trực tuyến, là nơi người dùng có thể nghe, xem và tải các bài hát, video âm nhạc thuộc nhiều thể loại khác nhau với chất lượng âm thanh và hình ảnh cao nhất hoàn toàn miễn phí (Lossless, HD1080p). Các bài hát được kiểm tra kỹ lưỡng về chất lượng thông qua hệ thống kiểm tra tự động và đội ngũ chuyên viên nhằm mang lại sự trải nghiệm hoàn hảo đến người nghe.
+		<br/>
+	</p>
+	<table width="100%" cellspacing="0" cellpadding="5" border="0" class="border-bottom" style="margin-bottom: 10px; border-bottom: none">
+    <tbody>
+    	<tr>
+	      <td valign="top">
+
+	        <p>
+	        	Website: <a href="http://chiasenhac.vn">www.chiasenhac.vn</a><br />
+	        	> Liên hệ với chúng tôi: <font color="#FF2D55"><u>admin@chiasenhac.vn</u></font><br /><br />
+	        	<!-- a href="http://chiasenhac.vn/login.php">> <font color="#FF2D55"><u>Thỏa thuận sử dụng</font></u></a -->
+	        </p>
+	      </td>
+    	</tr>
+  </tbody></table>
+
+  
+</div>
+<p class="footer no-margin" style="font-size: 12px;">Bản quyền &copy; 2019 CSN Technology JSC.</p>
+</body>
+</html>
