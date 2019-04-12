2200d9267d4537559dcd3dc9d47f65f2637f57a2
CSN\admin <tt.hau94@gmail.com>
2019-04-12 11:34:32 +0700

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,19 +36,19 @@ class Kernel extends ConsoleKernel
         //          ->hourly();
 //        $schedule->call('\App\Http\Controllers\Sync\SolrSyncController@syncCover');
 
-        $schedule->command('music_listen_download:type real')->hourlyAt(3); // gán giá trị listen và download
+        $schedule->command('music_listen_download:type real')->hourlyAt(3)->emailOutputTo('tt.hau94@gmail.com'); // gán giá trị listen và download
         $schedule->command('music_listen_download:type today')->dailyAt('00:08');
 
         $schedule->command('bxh_cat:type cat today')->hourlyAt('00:04');  // bảng xếp hạng hôm nay, tính bằng download_today_0, chạy trước dailyAt('00:08');
         $schedule->command('bxh_cat:type cat week')->dailyAt('00:10');   // bảng xếp hạng trong tuần, chạy sau dailyAt('00:08');
 
         $schedule->command('album')->hourlyAt(12)->emailOutputTo('tt.hau94@gmail.com');
-        $schedule->command('album_cat')->hourlyAt(15);
-        $schedule->command('music_download')->hourlyAt(20);
-        $schedule->command('suggestion_cat')->hourlyAt(25);
+        $schedule->command('album_cat')->hourlyAt(15)->emailOutputTo('tt.hau94@gmail.com');
+        $schedule->command('music_download')->hourlyAt(20)->emailOutputTo('tt.hau94@gmail.com');
+        $schedule->command('suggestion_cat')->hourlyAt(25)->emailOutputTo('tt.hau94@gmail.com');
 
-        $schedule->command('uploader:type 7day')->dailyAt('00:15');   // bảng xếp hạng uploader
-        $schedule->command('uploader:type cache_upload')->dailyAt('00:17');   // xóa files cache upload
+        $schedule->command('uploader:type 7day')->dailyAt('00:15')->emailOutputTo('tt.hau94@gmail.com');   // bảng xếp hạng uploader
+        $schedule->command('uploader:type cache_upload')->dailyAt('00:17')->emailOutputTo('tt.hau94@gmail.com');   // xóa files cache upload
 
 
         $schedule->command('bxh_cat:type cat_month ' . CURRENT_MONTH . ' 2019')->dailyAt('00:20');  // bảng xếp hạng tháng trong năm, (all sẽ lấy tất cả trong năm, all_month sẽ lấy tất cả tháng, số sẽ lấy đúng tháng)
