6546cb94bf9df82fcb2d2741341834319a11d505
CSN\admin <tt.hau94@gmail.com>
2019-04-16 14:57:35 +0700

    no message

--- resources/views/mobile/layouts/header.blade.php
+++ resources/views/mobile/layouts/header.blade.php
@@ -1,4 +1,5 @@
 <head>
+    <base href="{{URL::to('/')}}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <title>{{ $titleMeta or Config::get('constants.app.title') }}</title>
