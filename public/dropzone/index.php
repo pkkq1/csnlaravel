8c8f732c768b4fad05628ca1486dd472bd818848
CSN\admin <tt.hau94@gmail.com>
2019-04-19 11:23:28 +0700

    dropzone demo

---
+++ public/dropzone/index.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+
+    <!-- 1 -->
+    <link href="/dropzone/dist/dropzone.css" type="text/css" rel="stylesheet" />
+
+    <!-- 2 -->
+    <script src="/dropzone/dist/dropzone.js"></script>
+
+</head>
+
+<body>
+
+<!-- 3 -->
+<form action="/dropzone/upload.php" class="dropzone"></form>
+
+</body>
+
+</html>
