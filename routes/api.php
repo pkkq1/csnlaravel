ecd70efe82422614fe1c6ee40af7b75be8f3bd9b
CSN\admin <tt.hau94@gmail.com>
2019-04-26 17:23:44 +0700

    no message

--- routes/api.php
+++ routes/api.php
@@ -26,10 +26,14 @@ Route::prefix('auth')->group(function () {
 Route::get('search', 'SearchController@index');
 Route::post('nghe-album/{musicUrl}.html', ['as' => 'api.music.listen', 'uses' => 'MusicController@getMusicInfo']);
 Route::post('nghe-album/{musicUrl}', ['as' => 'api.music.listen', 'uses' => 'MusicController@getMusicInfo']);
-Route::post('artist/profile', ['as' => 'api.artist.profile', 'uses' => 'ArtistController@getProfile']);
-Route::get('artist/tab', ['as' => 'api.artist.tab', 'uses' => 'ArtistController@getTabArtist']);
-Route::post('artist/favourite', ['as' => 'api.artist.favourite', 'uses' => 'ArtistController@favourite']);
-Route::post('artist/list_all_music', ['as' => 'api.artist.list_all_music', 'uses' => 'ArtistController@listMusic']);
+
+// artist
+Route::prefix('artist')->group(function () {
+    Route::post('profile', ['as' => 'api.artist.profile', 'uses' => 'ArtistController@getProfile']);
+    Route::get('tab', ['as' => 'api.artist.tab', 'uses' => 'ArtistController@getTabArtist']);
+    Route::post('favourite', ['as' => 'api.artist.favourite', 'uses' => 'ArtistController@favourite']);
+    Route::post('list_all_music', ['as' => 'api.artist.list_all_music', 'uses' => 'ArtistController@listMusic']);
+});
 
 
 
