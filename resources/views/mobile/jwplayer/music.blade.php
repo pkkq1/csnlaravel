c9bbaac118e40dd74c2aa255638351c273e7aba1
TUANTRINH <trinhlt@gmail.com>
2019-05-02 17:14:32 +0700

    no message

--- resources/views/mobile/jwplayer/music.blade.php
+++ resources/views/mobile/jwplayer/music.blade.php
@@ -1242,4 +1242,3 @@ if($musicSet['type_listen'] == 'playlist') {
 </style>
 @endsection
 
-<iframe frameborder="0" allowtransparency="true" height="0" width="0" marginheight="0" marginwidth="0" vspace="0" hspace="0" src="https://hb.gammaplatform.com/adx/usersync"></iframe>
