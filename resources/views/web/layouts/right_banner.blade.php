cfb628956b525636a75c3f4d4bd563d50201b932
TUANTRINH <trinhlt@gmail.com>
2019-05-21 16:03:06 +0700

    ads

--- resources/views/web/layouts/right_banner.blade.php
+++ resources/views/web/layouts/right_banner.blade.php
@@ -1,23 +1,31 @@
 <?php
     $catalog = config('constants.catalog');
 ?>
+
     <div id="right1_banner">
-        <!-- BlueSeed - asynchronous code for placement 2043 Chiasenhac Right Banner -->
-        <ins class="hadar-placement" data-hdpm="2043" ></ins><script id="jshd1pm_2043" src="https://static.blueseed.tv/ajs/display.js"></script>
+        @if ($category->cat_id == 6)
+            <a href="https://www.facebook.com/groups/csn.kpop.fan/" target="_blank"><img src="/images/banner/kpop-fbgroup-300x170.jpg" width="300" height="170"></a>
+        @else
+            <!-- BlueSeed - asynchronous code for placement 2043 Chiasenhac Right Banner -->
+            <ins class="hadar-placement" data-hdpm="2043" ></ins><script id="jshd1pm_2043" src="https://static.blueseed.tv/ajs/display.js"></script>
+        @endif
     </div>
 
+    <br>
+
     @if(isset($short))
+        <?php $id = 1; ?>
         @foreach($catalog as $key => $val)
-            <a class="catalog1 weekend" style="background-image: url('/imgs/chude/{{$val['img_web']}}')" href="/chu-de/{{$key}}.html" title="Romance">
+            <a class="catalog1 weekend" style="background-image: url('/imgs/chude/{{$val['img_web']}}')" href="/chu-de/{{$key}}.html" title="{{$val['label']}}">
                 <span>{{$val['label']}}</span>
             </a>
-            @if($key == 2)
+            @if(++$id > 2)
                 @break;
             @endif
         @endforeach
     @else
         @foreach($catalog as $key => $val)
-            <a class="catalog1 weekend" style="background-image: url('/imgs/chude/{{$val['img_web']}}')" href="/chu-de/{{$key}}.html" title="Romance">
+            <a class="catalog1 weekend" style="background-image: url('/imgs/chude/{{$val['img_web']}}')" href="/chu-de/{{$key}}.html" title="{{$val['label']}}">
                 <span>{{$val['label']}}</span>
             </a>
         @endforeach
