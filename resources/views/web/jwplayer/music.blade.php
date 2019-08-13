0ab746c2fdcaa1ece6a764eda498b1593bafa1bb
TUANTRINH <trinhlt@gmail.com>
2019-08-13 11:07:57 +0700

    no message

--- resources/views/web/jwplayer/music.blade.php
+++ resources/views/web/jwplayer/music.blade.php
@@ -409,11 +409,12 @@ if($musicSet['type_listen'] == 'playlist') {
                     </div>
                 </div>
 
-                <script type="text/javascript" src="https://cdn.innity.net/admanager.js"></script>
-                <!-- Zone Tag : Chiasenhac 728x90-->
-                <script type="text/javascript">
-                    new innity_adZone("79c662560b0a5f1ae00b623ad8c775e3", "87808", {"origin": "VN","width": "728", "height": "90"});
-                </script>
+                <div class="row">
+                    <script type="text/javascript" src="https://cdn.innity.net/admanager.js"></script>
+                    <script type="text/javascript">
+                        new innity_adZone("79c662560b0a5f1ae00b623ad8c775e3", "87808", {"origin": "VN","width": "728", "height": "90"});
+                    </script>
+                </div>
 
                 @if($MusicSameArtist)
                     <div class="box_header d-flex justify-content-between align-items-end">
