0af6d8d1cd6ee2d7beb6040c252cccbfbbb52645
TUANTRINH <trinhlt@gmail.com>
2019-08-15 12:36:36 +0700

    data

--- app/Http/Controllers/Sync/SolrSyncController.php
+++ app/Http/Controllers/Sync/SolrSyncController.php
@@ -58,14 +58,12 @@ class SolrSyncController extends Controller
         } elseif ($time) {
             $searchMusic = MusicModel::select('music_id', 'music_composer', 'music_title', 'music_artist', 'music_downloads_this_week', 'music_search_result',
                 'cat_id', 'cat_level', 'cat_sublevel', 'cover_id', 'music_artist_id', 'music_album', 'music_listen', 'music_downloads', 'music_filename', 'music_bitrate', 'music_downloads_today', 'music_downloads_max_week', 'music_downloads_this_week', 'music_lyric', 'music_title_url', 'music_download_time')
-                ->where([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1], ['music_id', '<', 1416001], [$field, '>', $time]])
-                ->orwhere([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1], ['music_id', '>', 1419000], [$field, '>', $time]])
+                ->where([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1], [$field, '>', $time]])
                 ->get();
         } elseif ( $limit > 0 ) {
                 $searchMusic = MusicModel::select('music_id', 'music_composer', 'music_title', 'music_artist', 'music_downloads_this_week', 'music_search_result',
                     'cat_id', 'cat_level', 'cat_sublevel', 'cover_id', 'music_artist_id', 'music_album', 'music_listen', 'music_downloads', 'music_filename', 'music_bitrate', 'music_downloads_today', 'music_downloads_max_week', 'music_downloads_this_week', 'music_lyric', 'music_title_url', 'music_download_time')
-                    ->where([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1], ['music_id', '<', 1416001]])
-                    ->orwhere([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1], ['music_id', '>', 1419000]])
+                    ->where([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1]])
                     ->offset($offset)
                     ->limit($limit)
                     ->orderBy('music_id', 'asc')
@@ -81,8 +79,7 @@ class SolrSyncController extends Controller
             if (Auth::check() && (Auth::user()->id == 3 || Auth::user()->id == 997917)) {
                 $searchMusic = MusicModel::select('music_id', 'music_composer', 'music_title', 'music_artist', 'music_downloads_this_week', 'music_search_result',
                     'cat_id', 'cat_level', 'cat_sublevel', 'cover_id', 'music_artist_id', 'music_album', 'music_listen', 'music_downloads', 'music_filename', 'music_bitrate', 'music_downloads_today', 'music_downloads_max_week', 'music_downloads_this_week', 'music_lyric', 'music_title_url', 'music_download_time')
-                    ->where([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1], ['music_id', '>', intval($_GET['m_start'])], ['music_id', '<', 1416001]])
-                    ->orwhere([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1], ['music_id', '>', intval($_GET['m_start'])], ['music_id', '>', 1419000]])
+                    ->where([['cat_id', '!=', CAT_VIDEO], ['music_deleted', '<', 1], ['music_id', '>', intval($_GET['m_start'])]])
 //                ->whereIn('music_id', [1980711])
                     ->offset(0)
                     ->limit(50000)
@@ -115,7 +112,7 @@ class SolrSyncController extends Controller
 
         $datas = [];
         foreach ($searchMusic as $key => $item) {
-            if ($item->music_id < 1416001 or $item->music_id > 1419000) {
+            //if ($item->music_id < 1416001 or $item->music_id > 1419000) {
                 $titleSearch = Helpers::replaceKeySearch($item->music_title);
                 $artistSearch = Helpers::replaceKeySearch($item->music_artist);
                 $titleCharset = htmlspecialchars(Helpers::strReplaceSolr(Helpers::khongdau($titleSearch, ' ')), ENT_QUOTES);
@@ -182,7 +179,7 @@ class SolrSyncController extends Controller
                         echo ($key) . '/ ' . $item->music_id . "\n <br>";
                     }
                 }
-            }
+            //}
         }
         $this->Solr->addMultiDocuments($datas);
         //$this->Solr->solrMultiDeleteById($datas);
