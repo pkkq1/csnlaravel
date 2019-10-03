dffbb21cc83503e6a008d21aae7b35db4e635c79
TUANTRINH <trinhlt@gmail.com>
2019-10-03 17:08:39 +0700

    error ban quyen am nhac

--- resources/views/web/errors/403.blade.php
+++ resources/views/web/errors/403.blade.php
@@ -6,7 +6,12 @@
             <div class="col-lg-12">
                 <div class="centering text-center error-container">
                     <div class="text-center">
-                        <h2 class="without-margin"><span class="text-danger"><big><?php echo $message ?? 'Bạn không có quyền truy cập, thoát ra ngay :((' ?></big></span></h2>
+                        <h2 class="without-margin"><span class="text-danger">
+                                <?php
+                                    if ( stristr($message, 'bản quyền') ) $message = 'CSN chưa được cấp bản quyền đăng tải bài hát này.<br>Các bạn vui lòng không upload!<br><br>';
+                                    echo $message ?? 'Bạn không có quyền truy cập, thoát ra ngay :(('
+                                ?>
+                            </span></h2>
                         <h4 class="text-danger">Vui lòng liên hệ CSN để được hỗ trợ</h4>
                         <h4 class="text-danger">403</h4>
                     </div>
