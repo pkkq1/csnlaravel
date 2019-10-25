4038b6c32b6d8de9505e98d6547830890dcbcb43
CSN\admin <tt.hau94@gmail.com>
2019-10-25 11:28:13 +0700

    no message

--- resources/views/mobile/jwplayer/music.blade.php
+++ resources/views/mobile/jwplayer/music.blade.php
@@ -645,18 +645,14 @@ $memberVip = Helpers::checkMemberVip();
                 }
             }
         });
-        jwplayer('csnplayerads').play(false);
-        jwplayer('csnplayer').play(true);
-        jwplayer().play(true);
+ 
 
         var video_preroll_played = false;
         window.addEventListener('load', function () {
             document.body.addEventListener('touchstart', function (ae) {
                 if (video_preroll_played == false) {
                     video_preroll_played = true;
-                    jwplayer('csnplayerads').play(false);
-                    jwplayer('csnplayer').play(true);
-                    jwplayer().play(true);
+
                 }
             }, false)
         }, false);
