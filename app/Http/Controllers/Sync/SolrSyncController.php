930db1dc24517c4b8523da4fd922b8c6466c1ea0
TUANTRINH <trinhlt@gmail.com>
2019-11-12 14:52:12 +0700

    no message

--- app/Http/Controllers/Sync/SolrSyncController.php
+++ app/Http/Controllers/Sync/SolrSyncController.php
@@ -113,7 +113,7 @@ class SolrSyncController extends Controller
 
         $datas = [];
         foreach ($searchMusic as $key => $item) {
-            if ($item->music_id > 900000) {
+            if ($item->music_id > 200000) {
             if ($item->cat_id != CAT_VIDEO) {
                 $titleSearch = Helpers::replaceKeySearch($item->music_title);
                 $artistSearch = Helpers::replaceKeySearch($item->music_artist);
