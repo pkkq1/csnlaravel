c595c02addd3be31b5dfed301118e57ecc650cf2
TUANTRINH <trinhlt@gmail.com>
2019-12-05 14:14:35 +0700

    ads

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -115,5 +115,15 @@ if($memberVip == null)
     @endif
 @endif
 
+<script type="text/javascript">
+    window._urq = window._urq || [];
+    _urq.push(['initSite', '64a158d0-87a5-4e3d-a4dd-5518adf94d93']);
+    (function() {
+        var ur = document.createElement('script'); ur.type = 'text/javascript'; ur.async = true;
+        ur.src = ('https:' == document.location.protocol ? 'https://cdn.userreport.com/userreport.js' : 'http://cdn.userreport.com/userreport.js');
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ur, s);
+    })();
+</script>
+
 </body>
 </html>
