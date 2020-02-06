fed0a803603889812980631bdffc4ea049dc15c9
CSN\admin <tt.hau94@gmail.com>
2020-02-06 15:28:54 +0700

    no message

--- app/Repositories/Upload/UploadEloquentRepository.php
+++ app/Repositories/Upload/UploadEloquentRepository.php
@@ -73,6 +73,7 @@ class UploadEloquentRepository extends EloquentRepository implements UploadRepos
     public function musicByStage($stageArr, $fillOrder, $typeOrder, $page, $user = null)
     {
         $result = $this->_model::select('music_id', 'cat_id', 'cat_level', 'cover_id', 'music_title', 'music_artist', 'music_artist_id', 'music_album_id', 'music_bitrate', 'music_filename', 'music_updated', 'music_last_update_time', 'music_note')
+            ->where('music_time', '>=', time() - 2592000)
             ->whereIn('music_state', $stageArr);
         if($user) {
             $result = $result->where('music_last_update_by', $user);
