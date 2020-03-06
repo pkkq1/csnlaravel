e27c0055806f6e14a57ada2877db5ff81df46993
CSN\admin <tt.hau94@gmail.com>
2020-03-06 15:48:54 +0700

    no message

--- resources/views/mobile/jwplayer/music.blade.php
+++ resources/views/mobile/jwplayer/music.blade.php
@@ -84,7 +84,7 @@ $memberVip = Helpers::checkMemberVip();
                                 <div id="companion_cover">
                                     <div id="bg_blue" style="display: none; background: url('{{$thumnailMusic}}') no-repeat center;background-size: cover;padding-bottom: 70%;"
                                      class="image bg-blur"></div>
-                                    <div style="background: url('{{Helpers::coverThumb(Helpers::cover_url($item['cover_id']))}}') no-repeat center;background-size: cover;padding: 20%;"
+                                    <div style="background: url('{{Helpers::cover_url($music['cover_id'])}}') no-repeat center;background-size: cover;padding: 20%;"
                                      class="image-main"></div>
                                 </div>
                                 <div id="csnplayerads" style="position:relative; z-index: 99999; width:100%;"> </div>
