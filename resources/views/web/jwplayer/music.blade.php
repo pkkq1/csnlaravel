a4b484bbe08d4e623744d5cb8d5e2d8e672e668f
TUANTRINH <trinhlt@gmail.com>
2020-03-12 11:07:40 +0700

    remove ; keyword

--- resources/views/web/jwplayer/music.blade.php
+++ resources/views/web/jwplayer/music.blade.php
@@ -64,7 +64,7 @@ if($musicSet['type_listen'] == 'playlist') {
     <meta name="author" content="{{$music->music_username}}">
     <meta name="copyright" content="{{env('APP_URL')}}" />
     <meta name="revisit-after" content="7 Days">
-    <meta name="keywords" content="{{$music->music_title}}, {{$music->music_artist}}, <?php echo $musicSet['type_jw'] == 'video' ? 'HD 1080p, HD 720p, MV 480p, MV 360p, video, mv, hd, mp4' : 'Lossless, 500kbps, 320kbps, music, mp3, m4a, flac, lossless' ?>, csn, nghe nhac, tai nhac, loi bai hat, lyrics">
+    <meta name="keywords" content="{{$music->music_title}}, {{str_replace(";", ",", $music->music_artist)}}, <?php echo $musicSet['type_jw'] == 'video' ? 'HD 1080p, HD 720p, MV 480p, MV 360p, video, mv, hd, mp4' : 'Lossless, 500kbps, 320kbps, music, mp3, m4a, flac, lossless' ?>, csn, nghe nhac, tai nhac, loi bai hat, lyrics">
     <meta name="description" content="<?php echo $musicSet['type_jw'] == 'video' ? 'Nghe online video '.$music->music_title.' do ca sĩ '.$music->music_artist.' thể hiện. Tải nhạc dạng hd, mp4 và xem lời bài hát '.$music->music_title.' hoàn toàn miễn phí.' : 'Nghe online bài hát '.$music->music_title.' do ca sĩ '.$music->music_artist.' thể hiện. Tải nhạc dạng mp3, m4a, lossless và xem lời bài hát '.$music->music_title.' hoàn toàn miễn phí.' ?>">
     <link rel="canonical" href="<?php echo Helpers::listen_url($music, true) ?>" />
     <link rel="image_src" href="{{$thumnailMeta}}" />
