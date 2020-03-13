b456cca39dc690aa6331ecb55186f7bab89fa5a8
TUANTRINH <trinhlt@gmail.com>
2020-03-13 18:08:43 +0700

    giay phep mang xa hoi

--- resources/views/web/layouts/footer.blade.php
+++ resources/views/web/layouts/footer.blade.php
@@ -703,10 +703,10 @@
         <div class="row align-items-end">
             <div class="col-5 offset-2">
                 <address class="m-0">
-                    <div>Công ty Cổ phần giải trí Yêu Ca Hát</div>
+                    <div>Công ty cổ phần công nghệ CSN</div>
                     <div>Chủ sở hữu website: Ông Nguyễn Hoàng Minh Quân</div>
-                    <div>Giấy phép MXH số 337/GP-BTTTT do Bộ TT&TT cấp ngày 22/06/2016</div>
-                    <div>Giấy Chứng nhận Đăng ký Kinh doanh số 0305477372 do Sở kế hoạch và Đầu tư thành phố Hồ Chí Minh cấp ngày 21/01/2008.</div>
+                    <div>Giấy phép MXH số 92/GP-BTTTT do Bộ TT&TT cấp ngày 11/03/2020</div>
+                    <div>Giấy Chứng nhận Đăng ký Kinh doanh số 0314660744 do Sở kế hoạch và Đầu tư thành phố Hồ Chí Minh cấp ngày 05/10/2017.</div>
                 </address>
             </div>
             <div class="col-5">
