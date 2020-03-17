740a0edfec52238e1a6184348af75acf91baecc4
CSN\admin <tt.hau94@gmail.com>
2020-03-17 13:53:36 +0700

    no message

--- resources/views/web/jwplayer/music.blade.php
+++ resources/views/web/jwplayer/music.blade.php
@@ -1226,7 +1226,24 @@ if($musicSet['type_listen'] == 'playlist') {
                 displaySub.css('display', 'block');
             }
         }
-
+        //////////////////
+         /// FACEBOOK/////////////
+        ///////////
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId            : '<?php echo env('FACEBOOK_APP_ID') ?>',
+                autoLogAppEvents : true,
+                xfbml            : true,
+                version          : 'v3.1'
+            });
+        };
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
         //////////////////////////
         //////Comment///////////////
         ////////////////////////////
