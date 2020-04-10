ba6ec9d1b1a3df3cb178113d1d3ed6d8608d1141
CSN\admin <tt.hau94@gmail.com>
2020-04-10 13:02:36 +0700

    no message

--- resources/views/sitemap/sitemap_new_video.blade.php
+++ resources/views/sitemap/sitemap_new_video.blade.php
@@ -1,7 +1,7 @@
 <?php
 use App\Library\Helpers;
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">';
-foreach ($video as $item) {
+foreach ($videos as $item) {
     echo '<url>
 		<loc>' . $item['music_url'] . '</loc>
 		<video:video>
