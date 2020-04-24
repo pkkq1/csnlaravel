f5467319d7295b541df1be8a15b190c3d6405105
CSN\admin <tt.hau94@gmail.com>
2020-04-24 14:48:53 +0700

    no message

---
+++ resources/views/web/user/report_user/report_children.blade.php
@@ -0,0 +1,28 @@
+<?php
+use App\Library\Helpers;
+?>
+<div class="media" id="comment-{{$reply['comment_reply_id']}}">
+    <a href="/user/{{$reply['user']['id']}}" title="{{$reply['user']['name']}}"><img class="mr-3" src="<?php echo Helpers::pathAvatar($reply['user']['user_avatar'], $reply['user']['id']) ?>" alt="{{$reply['user']['user_avatar']}}"></a>
+    <div class="media-body">
+        <div class="d-flex align-items-center justify-content-between body_commnet_replay">
+            <h5 class="media-title mt-0 mb-1"><a href="/user/{{$reply['user']['id']}}" title="{{$reply['user']['name']}}">{{$reply['user']['name']}}</a>
+                @if($reply['comment_jw_postion'])
+                    at <span class="seek-jw" data-postion="{{$reply['comment_jw_postion']}}">{{ $reply['comment_jw_postion'] >= 3600 ? gmdate("H:i:s", $reply['comment_jw_postion']) : gmdate("i:s", $reply['comment_jw_postion'])}}</span>
+                @endif
+            </h5>
+            <time class="comment_time"><?php echo Helpers::timeElapsedString($reply['comment_time']); ?></time>
+            @if(Auth::check() && backpack_user()->can('comment_(can_block)'))
+                <div class="dropdown">
+                    <a class="comment_delete" href="javascript:void(0)" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">...</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item deleteComment" href="javascript:void(0)" onclick="deleteComment('comment', {{$item['comment_id']}}, 'delete')">Xóa</a>
+                        <a class="dropdown-item actionComment" href="javascript:void(0)" onclick="actionComment('comment', {{$item['comment_id']}}, '{{$item['comment_delete'] ? 'restore' : 'hidden'}}')">{{$item['comment_delete'] ? 'Phục hồi' : 'Ẩn'}}</a>
+                    </div>
+                </div>
+            @else
+            <a class="comment_report" href="javascript:void(0)" onclick="reportComment('comment-reply', {{$reply['comment_reply_id']}})">...</a>
+            @endif
+        </div>
+        <p class="media-text"><?php echo $reply['comment_delete'] ? '<i>Bình luận này đã bị ẩn.</i>' : $reply['comment_text'] ?></p>
+    </div>
+</div>
