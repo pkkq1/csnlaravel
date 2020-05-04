3deba06a1af7837d91c70541f24e0c0e27a6612d
CSN\admin <tt.hau94@gmail.com>
2020-05-04 14:02:01 +0700

    no message

--- resources/views/mobile/layouts/header.blade.php
+++ resources/views/mobile/layouts/header.blade.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta http-equiv="Content-Language" content="vi" />
     <title>{{ $titleMeta or Config::get('constants.app.title') }}</title>
+    @yield('meta')
     <link rel="icon" href="{{URL::to('/')}}/imgs/favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,400italic,300italic' rel='stylesheet' type='text/css'>
