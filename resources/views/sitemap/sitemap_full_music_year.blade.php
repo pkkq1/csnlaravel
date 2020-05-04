beb6dc5266ced6faec60c72f9e1de48cc5226fb8
CSN\admin <tt.hau94@gmail.com>
2020-05-04 15:53:47 +0700

    no message

--- resources/views/sitemap/sitemap_full_music_year.blade.php
+++ resources/views/sitemap/sitemap_full_music_year.blade.php
@@ -1,12 +1,10 @@
 <?php
 use App\Library\Helpers;
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+echo '<sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd" >';
 $pageTotal = ceil($music_page['row_total']/1000);
 for ($i = 1; $i <= $pageTotal; $i++) {
     echo '<url>
             <loc>' . env('APP_URL'). '/sitemap/full_2020_p' . $i . '.xml</loc>
-            <changefreq>always</changefreq>
-            <priority>0.9</priority>
             <lastmod>' . date("Y-m-d\TH:i:s+00:00") . '</lastmod>
         </url>';
 }
