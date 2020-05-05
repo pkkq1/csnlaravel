7bb27f501975ecd95a2df9bdee3d22c1c5e1ea68
TUANTRINH <trinhlt@gmail.com>
2020-05-05 11:07:11 +0700

    no message

--- app/Http/Controllers/Sitemap.php
+++ app/Http/Controllers/Sitemap.php
@@ -159,7 +159,7 @@ class Sitemap extends Controller
 //        $searchSolarium['music_id'] = '['.ID_OLD_MUSIC.' TO *] AND music_time :['.$timeSolr.' TO *]';
         //$searchSolarium['music_id'] = '['.ID_OLD_MUSIC.' TO *]';
         $searchSolarium['music_id'] = '['. (ID_OLD_MUSIC + (($page-1)*1000) + 1) .' TO '. (ID_OLD_MUSIC + ($page*1000)) .']';
-        $MusicPage = $this->Solr->search($searchSolarium);//, $page, 1000, array('score' => 'desc', 'id' => 'asc'));
+        $MusicPage = $this->Solr->search($searchSolarium, 1, 1000);//, $page, 1000, array('score' => 'desc', 'id' => 'asc'));
         return response()->view('sitemap.sitemap_category_solr_music', [
             'musics' => $MusicPage
         ])->header('Content-Type', 'text/xml');
