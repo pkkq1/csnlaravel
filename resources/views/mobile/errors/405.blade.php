13b7b561f76abc7026f48aece8563702297e1e18
CSN\admin <tt.hau94@gmail.com>
2020-05-08 17:47:10 +0700

    no message

--- resources/views/mobile/errors/405.blade.php
+++ resources/views/mobile/errors/405.blade.php
@@ -1,19 +1,35 @@
-@extends('web.layouts.app')
+@extends('mobile.layouts.app')
+
 @section('content')
-  <div class="container">
-    <div class="content">
-      <div class="title">405</div>
-      <div class="quote">Method not allowed.</div>
-      <div class="explanation">
-        <br>
-        <small>
-            <?php
-            $default_error_message = "Please return to <a href='".url('')."'>our homepage</a>.";
-            ?>
-          {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message !!}
-        </small>
+  <div class="header">
+    @include('mobile.layouts.header_top')
+  </div>
+  <main class="main main_player">
+    <div class="sidebar_top">
+      <div id="pills-tabContent" class="tab-content">
+        <div id="pills-thongtin" role="tabpanel" aria-labelledby="pills-thongtin-tab" class="tab-pane fade active show">
+          <div class="block_thongtin">
+            <div class="swiper-container swiper2 swiper-container-horizontal swiper-container-autoheight">
+              <div class="swiper-wrapper" style="height: 747px;">
+                <div class="swiper-slide swiper-slide-active" style="width: 375px;">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-lg-12">
+                        <div class="centering text-center error-container">
+                          <div class="text-center">
+                            <h2 class="without-margin"><span class="text-danger"><big>Method not allowed.</big></span></h2>
+                            <h4 class="text-danger">405</h4>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 @endsection
-
