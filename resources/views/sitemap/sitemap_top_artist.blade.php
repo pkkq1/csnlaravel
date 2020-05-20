5cc6daab70e49b98b6f21416623ae005b90367fc
CSN\admin <tt.hau94@gmail.com>
2020-05-20 15:54:59 +0700

    no message

--- resources/views/sitemap/sitemap_top_artist.blade.php
+++ resources/views/sitemap/sitemap_top_artist.blade.php
@@ -4,8 +4,6 @@ echo '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 foreach ($top_artist_rows as $item) {
     echo '<sitemap>
             <loc>' . env('APP_URL') . '/sitemap' . str_replace('.html', '.xml', $item['artist_url']) . '</loc>
-            <changefreq>always</changefreq>
-            <priority>0.9</priority>
             <lastmod>' . date("Y-m-d\TH:i:s+00:00") . '</lastmod>
         </sitemap>';
 }
