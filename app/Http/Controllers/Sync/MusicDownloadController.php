88f751d2d95edefd1b7c6d5b72c4cdd2f16061f0
CSN\admin <tt.hau94@gmail.com>
2020-06-03 10:32:29 +0700

    no message

--- app/Http/Controllers/Sync/MusicDownloadController.php
+++ app/Http/Controllers/Sync/MusicDownloadController.php
@@ -28,8 +28,7 @@ class MusicDownloadController extends Controller
     public function syncMusicDownload() {
         $cache = $this->musicRepository->getQueryPublished()
             ->where('cover_id', '>', 0)
-            ->select('music_id', 'music_title_url', 'music_title', 'music_artist', 'music_artist_id', 'cat_id', 'cat_level', 'cat_sublevel', 'cover_id', 'music_download_time', 'music_last_update_time', 'music_title_url',
-                'music_title_search', 'music_artist_search', 'music_album_search', 'music_composer', 'music_album', 'music_listen', 'music_track_id', 'music_track_id', 'music_filename', 'music_bitrate', 'music_shortlyric', 'music_last_update_time', 'music_time')
+            ->select('music_id', 'music_title_url', 'music_title', 'music_artist', 'music_artist_id', 'cat_id', 'cat_level', 'cat_sublevel', 'cover_id', 'music_download_time', 'music_last_update_time', 'music_title_url', 'music_artist_search', 'music_album_search', 'music_composer', 'music_album', 'music_listen', 'music_track_id', 'music_track_id', 'music_filename', 'music_bitrate', 'music_shortlyric', 'music_last_update_time', 'music_time')
             ->orderBy('music_download_time', 'desc')
             ->limit(20)->get();
         $download_rows = $cache->toArray();
--- app/Models/VideoFavouriteModel.php
+++ app/Models/VideoFavouriteModel.php
@@ -12,6 +12,6 @@ class VideoFavouriteModel extends Model
     protected $fillable = ['music_id', 'user_id'];
     public function video() {
         return $this->belongsTo('App\Models\VideoModel', 'music_id', 'music_id')->select('music_id', 'music_title_url', 'music_title', 'music_artist', 'music_artist_id', 'cat_id', 'cat_level', 'cat_sublevel', 'cover_id', 'music_download_time', 'music_last_update_time', 'music_title_url',
-            'music_title_search', 'music_artist_search', 'music_album_search', 'music_composer', 'music_album', 'music_listen', 'music_track_id', 'music_filename', 'music_filename', 'music_width', 'music_height', 'music_bitrate', 'music_length');
+             'music_artist_search', 'music_album_search', 'music_composer', 'music_album', 'music_listen', 'music_track_id', 'music_filename', 'music_filename', 'music_width', 'music_height', 'music_bitrate', 'music_length');
     }
 }
