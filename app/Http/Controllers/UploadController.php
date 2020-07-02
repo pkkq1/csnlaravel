ecde800ff3ce3bbf5b792ccdb4ce9cc82fd13523
CSN\admin <tt.hau94@gmail.com>
2020-07-02 18:25:28 +0700

    no message

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -863,7 +863,6 @@ class UploadController extends Controller
 
         $result = $result->toArray();
         $result['music_title'] = htmlspecialchars_decode($result['music_title'], ENT_QUOTES);
-        $result['music_album'] = htmlspecialchars_decode($result['music_album'], ENT_QUOTES);
         $result['music_lyric'] = htmlspecialchars_decode($result['music_lyric'], ENT_QUOTES);
         $result['music_composer'] = htmlspecialchars_decode($result['music_composer'], ENT_QUOTES);
         Helpers::ajaxResult(true, '', $result->toArray());
