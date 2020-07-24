033cac78a39621ecf1aab591e9a0bf54450b761d
TUANTRINH <trinhlt@gmail.com>
2020-07-24 17:47:41 +0700

    data

--- app/Library/helpers.php
+++ app/Library/helpers.php
@@ -1006,7 +1006,7 @@ class Helpers
             $url = 'https://data19.chiasenhac.com/';
         else if ($music_id <= 2085000)
             $url = 'https://data20.chiasenhac.com/';
-        else if ($music_id <= 2099000)
+        else if ($music_id <= 2104000)
             $url = 'https://data25.chiasenhac.com/';
         else
             $url = 'https://data.chiasenhac.com/';
