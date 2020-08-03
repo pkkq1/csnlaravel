c60ab53db9b1aa75ae419a1901574fce129c7160
CSN\admin <tt.hau94@gmail.com>
2020-08-03 13:42:41 +0700

    no message

--- resources/views/sitemap/sitemap_category_solr_music.blade.php
+++ resources/views/sitemap/sitemap_category_solr_music.blade.php
@@ -3,7 +3,7 @@ use App\Library\Helpers;
 echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">';
 foreach ($musics['data'] as $item) {
     echo '<url>
-        <loc>' . $item['music_link'][0] . '</loc>
+        <loc>' .  env('LISTEN_URL') . $item['music_link'][0] . '</loc>
         <video:video>
             <video:thumbnail_loc>' . $item['music_cover'][0] . '</video:thumbnail_loc>
             <video:title><![CDATA[' . htmlspecialchars_decode($item['music_title'][0], ENT_QUOTES) . ' - ' . htmlspecialchars_decode($item['music_artist'][0], ENT_QUOTES) . ']]></video:title>
