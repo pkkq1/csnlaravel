0f901f1a3a4cbfecdde0b4a49cc7cabca862652f
CSN\admin <tt.hau94@gmail.com>
2020-08-10 14:33:00 +0700

    no message

--- resources/views/mobile/registerVIP.blade.php
+++ resources/views/mobile/registerVIP.blade.php
@@ -18,6 +18,7 @@ $memberVip = Helpers::checkMemberVip();
     <meta property="og:type" content="website" />
     <meta property="og:updated_time" content="{{time()}}" />
 @endsection
+@section('no_ads', true)
 @extends('mobile.layouts.app')
 @section('content')
     <div class="header">
