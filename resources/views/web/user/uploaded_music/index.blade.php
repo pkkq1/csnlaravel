29b20b44603f057b687690c70186aa72651d11f3
CSN\admin <tt.hau94@gmail.com>
2020-08-12 16:17:02 +0700

    no message

--- resources/views/web/user/uploaded_music/index.blade.php
+++ resources/views/web/user/uploaded_music/index.blade.php
@@ -5,8 +5,8 @@
         <li class="warehouse_stage_delete da-xoa" data-key="delete"><a class="da-xoa" onclick="musicUploadTab('warehouse_stage_delete', 'delete', 'da-xoa')" href="javascript:void(0)"><span>Đã Xóa</span></a></li>
         <li class="warehouse_stage_album album" data-key="album"><a class="album" onclick="musicUploadTab( 'warehouse_stage_album', 'album', 'album')" href="javascript:void(0)"><span>Album</span></a></li>
         @if((Auth::check() && Auth::user()->hasPermission('duyet_sua_nhac')))
-        <li class="approval_stage_fullcensor_by da-duyet-boi" data-key="fullcensor_by"><a class="da-duyet-boi" onclick="musicUploadTab( 'approval_stage_fullcensor_by', 'fullcensor_by', 'da-duyet-boi')" href="javascript:void(0)"><span>Duyệt Bởi {{substr($user_name, 8)}}</span></a></li>
-        <li class="approval_stage_delete_by da-xoa-boi" data-key="delete_by"><a class="da-xoa-boi" onclick="musicUploadTab( 'approval_stage_delete_by', 'delete_by', 'da-xoa-boi')" href="javascript:void(0)"><span>Xóa Bởi {{substr($user_name, 8)}}</span></a></li>
+        <li class="approval_stage_fullcensor_by da-duyet-boi" data-key="fullcensor_by"><a class="da-duyet-boi" onclick="musicUploadTab( 'approval_stage_fullcensor_by', 'fullcensor_by', 'da-duyet-boi')" href="javascript:void(0)"><span><div style=" white-space: nowrap; width: 150px; overflow: hidden; text-overflow: ellipsis; line-height: 18px; ">Duyệt Bởi {{$user_name}}</div></span></a></li>
+        <li class="approval_stage_delete_by da-xoa-boi" data-key="delete_by"><a class="da-xoa-boi" onclick="musicUploadTab( 'approval_stage_delete_by', 'delete_by', 'da-xoa-boi')" href="javascript:void(0)"><span><div style=" white-space: nowrap; width: 150px; overflow: hidden; text-overflow: ellipsis; line-height: 18px; ">Duyệt Bởi {{$user_name}}</div></span></a></li>
         @endif
     </ul>
 </nav>
