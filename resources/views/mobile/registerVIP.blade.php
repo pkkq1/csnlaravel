e3d054f15e13afc063f8f42f2527f4d966bf56e4
TUANTRINH <trinhlt@gmail.com>
2020-08-20 10:23:47 +0700

    vip

--- resources/views/mobile/registerVIP.blade.php
+++ resources/views/mobile/registerVIP.blade.php
@@ -54,7 +54,7 @@ $titleMeta = 'Đăng ký VIP Chia Sẻ Nhạc';
                             <span style="font-size: 18px;">- Lời chúc (Tin nhắn): </span><span style="font-size: 18px; font-weight: 700; color: red">CSN &nbsp; {{Auth::user()->user_id}}</span><br>
                             <span style="font-size: 18px;">- Số tiền: 60.000 VNĐ (tùy ý theo gói bạn muốn mua)</span><br>
                             <img style="width: 100%; display: inline-block;" src="/imgs/qr-code-vip-csn-60k.jpg">
-                            * Nếu bạn không sử dụng app Momo, bạn có thể nhờ bất kỳ người nào có sử dụng app Momo để chuyển tiền mua VIP giúp bạn, chỉ cần chuyển tiền theo đúng số tài khoản và nội dung lời chúc như trên.<br><br>
+                            * Nếu bạn không sử dụng app Momo, bạn có thể nhờ bất kỳ người nào có sử dụng app Momo để chuyển tiền mua VIP giúp bạn, chỉ cần chuyển tiền theo đúng số tài khoản và nội dung lời chúc là <span style="font-size: 18px; font-weight: 700; color: red">CSN &nbsp;{{Auth::user()->user_id}}</span> như trên.<br><br>
                             * Vui lòng vui lòng không gọi qua số điện thoại này, CSN sẽ chỉ hỗ trợ thắc mắc trong khung <a target="_blank" href="/user/{{Auth::user()->id}}?tab=message_csn">liên hệ</a> bên dưới.<br>
                             <br />
                             Sau khi chuyển thành công, tài khoản của bạn cần chờ từ 5 phút đến 1 tiếng để được hệ thống tự động nâng cấp VIP. Nếu bạn nạp sai cú pháp hoặc sai số tiền thì tiền sẽ được hoàn lại vào ví Momo của bạn trong vòng 1-2 ngày. Nếu bạn nạp đúng quy định nhưng quá hạn thời gian mà tài khoản của bạn vẫn chưa lên VIP, vui lòng gửi thông tin liên hệ qua bảng bên dưới để BQT kiểm tra và xử lý.
