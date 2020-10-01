2847159ada3e345da5727baf68274aa452eec936
CSN\admin <tt.hau94@gmail.com>
2020-10-01 11:55:07 +0700

    no message

--- app/Http/Middleware/CheckRequestApi.php
+++ app/Http/Middleware/CheckRequestApi.php
@@ -4,11 +4,13 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\App;
 use Config;
+use \Illuminate\Http\JsonResponse;
 
 class CheckRequestApi {
 
     public function handle($request, Closure $next)
     {
+        return new JsonResponse(['message' => 'Fail', 'code' => 400, 'data' => [], 'error' => 'Hiện tại các api đang bảo trì'], 400);
         return $next($request);
     }
 }
