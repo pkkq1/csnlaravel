0a44b565c70e6e86a0de170e6acdd5a928d6de8c
CSN\admin <tt.hau94@gmail.com>
2020-12-07 17:29:43 +0700

    no message

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -226,7 +226,6 @@ class MusicController extends Controller
             }
         }
         //set cookie key search
-        var_dump($_COOKIE['search_search'] ?? '');
         if(Auth::check() && Auth::user()->id == 997917) {
             var_dump($_COOKIE['search_search'] ?? '');
         }
