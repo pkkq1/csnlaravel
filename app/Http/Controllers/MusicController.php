533668bcc1b42a03e04cd1b03422ac5bb30c1d66
CSN\admin <tt.hau94@gmail.com>
2020-12-07 18:22:21 +0700

    no message

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -226,10 +226,9 @@ class MusicController extends Controller
             }
         }
         //set cookie key search
-//        if(Auth::check() && Auth::user()->id == 997917) {
-//            unset($_COOKIE['search_search']);
-//            var_dump($_COOKIE['search_search'] ?? '');
-//        }
+        if(Auth::check() && Auth::user()->id == 997917) {
+            var_dump($_COOKIE['search_search'] ?? '');
+        }
         if($_COOKIE['search_search'] ?? '') {
             unset($_COOKIE['search_search']);
             $search = htmlspecialchars_decode($music->music_title, ENT_QUOTES);
