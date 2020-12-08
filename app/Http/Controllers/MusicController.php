2b0be683152329f8ec4b47f5f38dea2c1f2311de
CSN\admin <tt.hau94@gmail.com>
2020-12-08 15:17:28 +0700

    no message

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -231,7 +231,7 @@ class MusicController extends Controller
 //            var_dump($_COOKIE['search_search'] ?? '');
 //        }
         if($_COOKIE['search_search'] ?? '') {
-            setcookie('search_search', null, -1, '/');
+            setcookie('search_search', null, time()-1000, '/', env('SESSION_DOMAIN'));
             $search = htmlspecialchars_decode($music->music_title, ENT_QUOTES);
             if(Auth::check()) {
                 $searchRecent = Auth::user()->user_music_search_recent;
@@ -455,7 +455,7 @@ class MusicController extends Controller
             }
         }
         if($_COOKIE['search_search'] ?? '') {
-            setcookie('search_search', null, -1, '/');
+            setcookie('search_search', null, time()-1000, '/', env('SESSION_DOMAIN'));
             $search = htmlspecialchars_decode($music->music_title, ENT_QUOTES);
             if(Auth::check()) {
                 $searchRecent = Auth::user()->user_music_search_recent;
