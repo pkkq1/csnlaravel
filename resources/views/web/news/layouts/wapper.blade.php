73d592fe51a2473147e60ec7dce987f52d0503a3
CSN\admin <tt.hau94@gmail.com>
2021-02-04 13:47:55 +0700

    no message

--- resources/views/web/news/layouts/wapper.blade.php
+++ resources/views/web/news/layouts/wapper.blade.php
@@ -9,9 +9,9 @@
                 <div rel="show--menu" class="c-list__menu--main">
                     <nav>
                         <ul class="u-flex--wrap u-flex__x--end">
-                            <li><a href="{{URL::to('/')}}/tin-tuc.html" title="Trang chủ"><i class="c-icon--home"></i><span>Trang chủ</span></a></li>
-                            <li><a href="{{URL::to('/')}}/tin-tuc/tin-noi-bat.html" title="Tin nổi bật"><span>Tin nổi bật</span></a></li>
-                            <li><a href="{{URL::to('/')}}/tin-tuc/tin-moi-nhat.html" title="Tin mới nhất"><span>Tin mới nhất</span></a></li>
+                            <li><a href="{{URL::to('/')}}/tin-tuc.html" title="Trang chủ"><i class="c-icon--home"></i><span>TRANG CHỦ</span></a></li>
+                            <li><a href="{{URL::to('/')}}/tin-tuc/tin-noi-bat.html" title="Tin nổi bật"><span>TIN NỖI BẬT</span></a></li>
+                            <li><a href="{{URL::to('/')}}/tin-tuc/tin-moi-nhat.html" title="Tin mới nhất"><span>TIN MỚI NHẤT</span></a></li>
                             @include('cache.news_category')
                         </ul>
                     </nav>
