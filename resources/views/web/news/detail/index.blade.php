3655ebc52e17c98353d76849aacdcd9430bcbc44
CSN\admin <tt.hau94@gmail.com>
2021-02-24 10:37:12 +0700

    no message

--- resources/views/web/news/detail/index.blade.php
+++ resources/views/web/news/detail/index.blade.php
@@ -197,10 +197,10 @@ $Agent = new Agent();
                     font-size: 45px;
                     line-height: 80px;
                 }
-                /*.c-wraper .c-main-content p img {*/
-                    /*width: 100%!important;*/
-                    /*height: auto!important;*/
-                /*}*/
+                .c-wraper .c-main-content p img {
+                    width: 100%!important;
+                    height: auto!important;
+                }
                 .c-list__container--03 .c-list__item .c-card__header {
                     width: 30%;
                 }
