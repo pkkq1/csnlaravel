1778d01912c144f9020c866822cc1cb5a649c60d
TUANTRINH <trinhlt@gmail.com>
2021-03-31 16:02:22 +0700

    data

--- app/Library/helpers.php
+++ app/Library/helpers.php
@@ -1031,7 +1031,7 @@ class Helpers
             $url = 'https://data3.chiasenhac.com/';
         else if ($music_id <= 2144000)
             $url = 'http://data16.chiasenhac.com/';
-        else if ($music_id <= 2159000)
+        else if ($music_id <= 2162000)
             $url = 'https://data25.chiasenhac.com/';
         else
             $url = 'https://data.chiasenhac.com/';
