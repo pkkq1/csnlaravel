9e4b8d19c6ffa177cda4c357dff6da32195e3a53
TUANTRINH <trinhlt@gmail.com>
2021-05-14 15:02:21 +0700

    no message

--- resources/views/web/jwplayer/music.blade.php
+++ resources/views/web/jwplayer/music.blade.php
@@ -894,7 +894,7 @@ if( !$memberVip && !$isVNIP )
             }
             ?>
 
-                <?php if($pc_audio_vast[0] != '.') {
+                <?php if($pc_audio_vast[0] != '.' && $music->music_id == 1111000) {
                         echo '
             advertising: {
                 client: "vast",
