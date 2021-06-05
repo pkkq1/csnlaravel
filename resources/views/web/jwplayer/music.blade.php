6c608a722364e85e00b0d9c90f721de6ffca6d11
TUANTRINH <trinhlt@gmail.com>
2021-06-05 17:21:43 +0700

    no message

--- resources/views/web/jwplayer/music.blade.php
+++ resources/views/web/jwplayer/music.blade.php
@@ -895,7 +895,7 @@ if( !$memberVip && !$isVNIP )
                 },
                 ';
             }
-            else if(!$memberVip && $pc_audio_vast[0] != '.' && rand(1,2) == 1) {
+            else if(!$memberVip && $pc_audio_vast[0] != '.') {
                         echo '
             advertising: {
                 client: "vast",
