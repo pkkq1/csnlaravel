ff29b301c727b4b332d95c3e2b4b3c3b0f94567d
TUANTRINH <trinhlt@gmail.com>
2022-10-04 14:25:55 +0700

    no message

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -384,7 +384,7 @@ $catalog = config('constants.catalog');
                     <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
                         <ul class="list-unstyled bxh mb-0">
                             <?php
-                            $catMusic = array_slice($hot_music_rows[4], 0, LIMIT_HOME_CAT_MUSIC);
+                            $catMusic = array();//array_slice($hot_music_rows[4], 0, LIMIT_HOME_CAT_MUSIC);
                             array_map(function ($i, $item) {
                             $musicId = Helpers::music_id($item);
                             $url = SUB_BXH_NOW_MUSIC.'us-uk.html?playlist='.++$i;
