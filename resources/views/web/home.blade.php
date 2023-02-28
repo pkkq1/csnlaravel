e133765a504f9d42d911ce97b69a7a2300e823eb
TUANTRINH <trinhlt@gmail.com>
2023-03-01 00:08:22 +0700

    no message

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -444,7 +444,7 @@ $catalog = config('constants.catalog');
                     <div class="tab-pane fade" id="jpop" role="tabpanel" aria-labelledby="jpop-tab">
                         <ul class="list-unstyled bxh mb-0">
                             <?php
-                            $catMusic = (sizeof($hot_music_rows[7]) > LIMIT_HOME_CAT_MUSIC) ? array_slice($hot_music_rows[7], 0, LIMIT_HOME_CAT_MUSIC) : $hot_music_rows[7];
+                            $catMusic = isset($hot_music_rows[7]) ? ((sizeof($hot_music_rows[7]) > LIMIT_HOME_CAT_MUSIC) ? array_slice($hot_music_rows[7], 0, LIMIT_HOME_CAT_MUSIC) : $hot_music_rows[7]) : array();
                             array_map(function ($i, $item) {
                             $musicId = Helpers::music_id($item);
                             $url = SUB_BXH_NOW_MUSIC.'japan.html?playlist='.++$i;
